4b688f06df1e48e0268a9ed945a44d855ea47a0e
idktrolled <camilocarlos1100@gmail.com>
2026-05-13 15:32:35 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3043,7 +3043,6 @@ class PlayState extends MusicBeatState
 	
 						if (SONG.validScore)
 						{
-							NGio.unlockMedal(60961);
 							Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
 						}
 	
